a4e9d00ffa150f05721237a7d02e1e3c1182e390
Arefin Ahmed <arefinahmed131@gmail.com>
2025-08-26 19:52:48 +0600

    Structure Programming Lab Final

--- 2nd Semester/Structured Programming Lab/Lab Final/2_no.c
+++ 2nd Semester/Structured Programming Lab/Lab Final/2_no.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
-
-int is_palindrome(char str[]) {
+int is_palindrome(char str[])
+{
     int len = strlen(str);
 
-    for (int left = 0, right = len - 1; left < right; left++, right--) {
+    for (int left = 0, right = len - 1; left < right; left++, right--)
+    {
         if (str[left] != str[right])
-            return 0; 
+            return 0;
     }
-    return 1; 
+    return 1;
 }
 
-int main() {
+int main()
+{
     char s[100];
     printf("Enter a string: ");
     scanf("%s", s);
 
-   
     if (is_palindrome(s))
         printf("Palindrome\n");
     else
