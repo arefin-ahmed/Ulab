45bac16c78bcc88fa2bb03d1a4813ed962d1d24e
Arefin Ahmed <arefinahmed131@gmail.com>
2025-10-15 22:36:58 +0600

    Array deletion

--- 3rd Semester/Structured Programming Lab/Array_deletion.c
+++ 3rd Semester/Structured Programming Lab/Array_deletion.c
@@ -12,5 +12,32 @@ int main()
         scanf("%d", &arr[i]);
     }
 
+    printf("Enter the position to delete: ");
+    scanf("%d", &pos);
+
+    if (pos >= 1 && pos <= n)
+    {
+        for (i = pos - 1; i < n - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+    n--;
+
+    printf("Array after deletion: ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    
+    printf("\n");
+
     return 0;
+    }
+    else
+    {
+        printf("Invalid position\n");
+        return 0;
+    }
+
+    
 }
