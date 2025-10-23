8726cc34262337ff8e42730f0f46f0f7ea3e08d4
Arefin Ahmed <arefinahmed131@gmail.com>
2025-10-23 11:15:20 +0600

    Bubble Sort

---
+++ 3rd Semester/Data Structure Lab/23. 10. 25/Bubble_Sort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+void BubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - i - 1; i++)
+    {
+        for (j = 0; j < n - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int swap = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = swap;
+            }
+        }
+    }
+}
+int main()
+{
+    int n, x;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    int arr[n];
+
+    printf("Enter %d integers:\n", n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    BubbleSort(arr, n);
+    printf("Sorted Elements: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
