25ff1120b882e0a70c2bf34dd34d9cfcaddfdc52
Arefin Ahmed <arefinahmed131@gmail.com>
2025-11-19 22:12:24 +0600

    stack practice

---
+++ 3rd Semester/Data Structure Lab/Assignment/queue_251014066.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#define SIZE 5
+
+int front = -1;
+int rear = -1;
+int queue[SIZE];
+
+// Function to add element to queue
+void enqueue(int n)
+{
+    if (rear == SIZE - 1)
+    {
+        printf("Sorry, the queue is full at the moment!\n");
+    }
+    else
+    {
+        if (front == -1)
+        { // If queue was empty
+            front = 0;
+        }
+        rear++;
+        queue[rear] = n;
+        printf("%d enqueued to the queue.\n", n);
+    }
+}
+
+// Function to remove element from queue
+void dequeue()
+{
+    if (front == -1 || front > rear)
+    {
+        printf("The queue is empty right now!\n");
+    }
+    else
+    {
+        int value = queue[front];
+        printf("Dequeued value is: %d\n", value);
+        front++;
+        if (front > rear)
+        { // Queue becomes empty
+            front = rear = -1;
+        }
+    }
+}
+
+// Function to display queue
+void printQueue()
+{
+    if (front == -1)
+    {
+        printf("Queue is empty!\n");
+    }
+    else
+    {
+        printf("Elements in the queue: ");
+        for (int i = front; i <= rear; i++)
+        {
+            printf("%d ", queue[i]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int ch;
+    do
+    {
+        printf("\nQueue Operations Menu:\n");
+        printf("1: Enqueue an element\n");
+        printf("2: Dequeue an element\n");
+        printf("3: Display all elements\n");
+        printf("4: Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &ch);
+
+        switch (ch)
+        {
+        case 1:
+        {
+            int item;
+            printf("Enter element to enqueue: ");
+            scanf("%d", &item);
+            enqueue(item);
+            break;
+        }
+        case 2:
+            dequeue();
+            break;
+        case 3:
+            printQueue();
+            break;
+        case 4:
+            printf("Exiting program.\n");
+            break;
+        default:
+            printf("Invalid choice! Try again.\n");
+        }
+    } while (ch != 4);
+
+    return 0;
+}
