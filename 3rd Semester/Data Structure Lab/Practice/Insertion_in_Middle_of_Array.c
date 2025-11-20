e2099b7d3c2910043aa3a37eeafcaad116fec01c
Arefin Ahmed <arefinahmed131@gmail.com>
2025-11-20 08:57:19 +0600

    Traversing an Array

--- 3rd Semester/Data Structure Lab/Practice/Insertion_in_Middle_of_Array.c
+++ 3rd Semester/Data Structure Lab/Practice/Insertion_in_Middle_of_Array.c
@@ -4,14 +4,12 @@ int main()
 {
     int i, n, num, pos, arr[100];
 
-    // Ask the user how many elements are currently in the array
     printf("\nEnter the number of elements in the array: ");
     scanf("%d", &n);
 
-    // Read all array elements from user
     for (i = 0; i < n; i++)
     {
-        printf("arr[%d] = ", i); // Display index
+        printf("arr[%d] = ", i);
         scanf("%d", &arr[i]);    // Store value at arr[i]
     }
 
@@ -52,3 +50,62 @@ int main()
 
     return 0;
 }
+
+
+
+
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i, n, num, pos, arr[100];
+
+//     // Ask the user how many elements are currently in the array
+//     printf("\nEnter the number of elements in the array: ");
+//     scanf("%d", &n);
+
+//     // Read all array elements from user
+//     for (i = 0; i < n; i++)
+//     {
+//         printf("arr[%d] = ", i); // Display index
+//         scanf("%d", &arr[i]);    // Store value at arr[i]
+//     }
+
+//     // Ask the user which number they want to insert
+//     printf("\nEnter the number to be inserted: ");
+//     scanf("%d", &num);
+
+//     // Ask the user the position where the new number should be inserted
+//     printf("Enter the position at which the number has to be added: ");
+//     scanf("%d", &pos);
+
+//     /*
+//        SHIFTING ELEMENTS TO THE RIGHT
+//        ---------------------------------
+//        Start from the last element (n-1)
+//        Move each element one position ahead
+//        Stop when we reach the insertion position
+//        This creates an empty space at arr[pos]
+//     */
+//     for (i = n - 1; i >= pos; i--)
+//     {
+//         arr[i + 1] = arr[i]; // Shift element to the right
+//     }
+
+//     // Insert the new number at the required position
+//     arr[pos] = num;
+
+//     // Increase array size because one element is added
+//     n = n + 1;
+
+//     // Display the final array after insertion
+//     printf("\nThe array after insertion of %d is:\n", num);
+
+//     for (i = 0; i < n; i++)
+//     {
+//         printf("arr[%d] = %d\n", i, arr[i]);
+//     }
+
+//     return 0;
+// }
--- 3rd Semester/Data Structure Lab/Practice/Traversing_an_Array.c
+++ 3rd Semester/Data Structure Lab/Practice/Traversing_an_Array.c
@@ -2,54 +2,26 @@
 
 int main()
 {
-    int i, n, arr[20];
+    int i, n, arr[20]; // i = loop counter, n = number of elements, arr = integer array of size 20
 
+    // Ask the user for the number of elements
     printf("\nEnter the number of elements in the array: ");
-    scanf("%d", &n);
+    scanf("%d", &n); // Read the value of n
 
+    // Input (Traverse array to read values)
     for (i = 0; i < n; i++)
     {
-        printf("arr[%d] = ", i);
-        scanf("%d", &arr[i]);
+        printf("arr[%d] = ", i); // Display index number
+        scanf("%d", &arr[i]);    // Store value in array
     }
 
+    // Output (Traverse array to display values)
     printf("\nThe array elements are: ");
 
     for (i = 0; i < n; i++)
     {
-        printf("%d\t", arr[i]);
+        printf("%d\t", arr[i]); // Print each element
     }
 
-    return 0;
+    return 0; // End of program
 }
-
-
-
-
-// #include <stdio.h>
-
-// int main()
-// {
-//     int i, n, arr[20]; // i = loop counter, n = number of elements, arr = integer array of size 20
-
-//     // Ask the user for the number of elements
-//     printf("\nEnter the number of elements in the array: ");
-//     scanf("%d", &n); // Read the value of n
-
-//     // Input (Traverse array to read values)
-//     for (i = 0; i < n; i++)
-//     {
-//         printf("arr[%d] = ", i); // Display index number
-//         scanf("%d", &arr[i]);    // Store value in array
-//     }
-
-//     // Output (Traverse array to display values)
-//     printf("\nThe array elements are: ");
-
-//     for (i = 0; i < n; i++)
-//     {
-//         printf("%d\t", arr[i]); // Print each element
-//     }
-
-//     return 0; // End of program
-// }
