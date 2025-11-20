6ddf2eb45493a1bbd95d7ba11cd48c379ffb8bc6
Arefin Ahmed <arefinahmed131@gmail.com>
2025-11-20 08:55:48 +0600

    Traversing an Array

--- 3rd Semester/Data Structure Lab/Practice/Traversing_an_Array.c
+++ 3rd Semester/Data Structure Lab/Practice/Traversing_an_Array.c
@@ -2,26 +2,54 @@
 
 int main()
 {
-    int i, n, arr[20]; // i = loop counter, n = number of elements, arr = integer array of size 20
+    int i, n, arr[20];
 
-    // Ask the user for the number of elements
     printf("\nEnter the number of elements in the array: ");
-    scanf("%d", &n); // Read the value of n
+    scanf("%d", &n);
 
-    // Input (Traverse array to read values)
     for (i = 0; i < n; i++)
     {
-        printf("arr[%d] = ", i); // Display index number
-        scanf("%d", &arr[i]);    // Store value in array
+        printf("arr[%d] = ", i);
+        scanf("%d", &arr[i]);
     }
 
-    // Output (Traverse array to display values)
     printf("\nThe array elements are: ");
 
     for (i = 0; i < n; i++)
     {
-        printf("%d\t", arr[i]); // Print each element
+        printf("%d\t", arr[i]);
     }
 
-    return 0; // End of program
+    return 0;
 }
+
+
+
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i, n, arr[20]; // i = loop counter, n = number of elements, arr = integer array of size 20
+
+//     // Ask the user for the number of elements
+//     printf("\nEnter the number of elements in the array: ");
+//     scanf("%d", &n); // Read the value of n
+
+//     // Input (Traverse array to read values)
+//     for (i = 0; i < n; i++)
+//     {
+//         printf("arr[%d] = ", i); // Display index number
+//         scanf("%d", &arr[i]);    // Store value in array
+//     }
+
+//     // Output (Traverse array to display values)
+//     printf("\nThe array elements are: ");
+
+//     for (i = 0; i < n; i++)
+//     {
+//         printf("%d\t", arr[i]); // Print each element
+//     }
+
+//     return 0; // End of program
+// }
