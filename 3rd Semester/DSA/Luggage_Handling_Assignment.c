cc281bff0586363d9f4e098ce738089a1492dd7b
Arefin Ahmed <arefinahmed131@gmail.com>
2025-11-24 00:39:36 +0600

    AIRPORT AUTOMATION MENU with Comment

--- 3rd Semester/DSA/Luggage_Handling_Assignment.c
+++ 3rd Semester/DSA/Luggage_Handling_Assignment.c
@@ -1,42 +1,59 @@
 #include <stdio.h>
-#define SIZE 5
+#include <stdlib.h>
+#define SIZE 50
 
 /* ---------------- STACK SECTION (Luggage) ---------------- */
 
 int stack[SIZE];
 int top = -1;
 
-void push(int tag) {
-    if (top == SIZE - 1) {
+void push(int tag)
+{
+    if (top == SIZE - 1)
+    {
         printf("Stack Overflow! Cannot add luggage.\n");
-    } else {
+    }
+    else
+    {
         top++;
         stack[top] = tag;
         printf("Luggage %d added successfully.\n", tag);
     }
 }
 
-void pop() {
-    if (top == -1) {
+void pop()
+{
+    if (top == -1)
+    {
         printf("Stack Underflow! No luggage to remove.\n");
-    } else {
+    }
+    else
+    {
         printf("Luggage %d removed.\n", stack[top]);
         top--;
     }
 }
 
-void peekStack() {
-    if (top == -1) {
+void peekStack()
+{
+    if (top == -1)
+    {
         printf("Stack is empty.\n");
-    } else {
+    }
+    else
+    {
         printf("Top luggage tag: %d\n", stack[top]);
     }
 }
 
-void displayStack() {
-    if (top == -1) {
+void displayStack()
+{
+    if (top == -1)
+    {
         printf("No luggage on the conveyor.\n");
-    } else {
+    }
+    else
+    {
         printf("Luggage on conveyor (Top to Bottom): ");
         for (int i = top; i >= 0; i--)
             printf("%d ", stack[i]);
@@ -49,13 +66,16 @@ void displayStack() {
 int queue[SIZE];
 int front = -1, rear = -1;
 
-void enqueue(int id) {
-    if ((front == 0 && rear == SIZE - 1) || (rear + 1 == front)) {
+void enqueue(int id)
+{
+    if ((front == 0 && rear == SIZE - 1) || (rear + 1 == front))
+    {
         printf("Queue Overflow! Passenger cannot enter.\n");
         return;
     }
 
-    if (front == -1) front = 0;
+    if (front == -1)
+        front = 0;
 
     rear = (rear + 1) % SIZE;
     queue[rear] = id;
@@ -63,8 +83,10 @@ void enqueue(int id) {
     printf("Passenger %d added to queue.\n", id);
 }
 
-void dequeue() {
-    if (front == -1) {
+void dequeue()
+{
+    if (front == -1)
+    {
         printf("Queue Underflow! No passenger to remove.\n");
         return;
     }
@@ -77,16 +99,22 @@ void dequeue() {
         front = (front + 1) % SIZE;
 }
 
-void peekQueue() {
-    if (front == -1) {
+void peekQueue()
+{
+    if (front == -1)
+    {
         printf("Queue is empty.\n");
-    } else {
+    }
+    else
+    {
         printf("Next passenger ID: %d\n", queue[front]);
     }
 }
 
-void displayQueue() {
-    if (front == -1) {
+void displayQueue()
+{
+    if (front == -1)
+    {
         printf("No passengers waiting.\n");
         return;
     }
@@ -94,20 +122,24 @@ void displayQueue() {
     printf("Passengers in queue (Front to Rear): ");
     int i = front;
 
-    while (1) {
+    while (1)
+    {
         printf("%d ", queue[i]);
-        if (i == rear) break;
+        if (i == rear)
+            break;
         i = (i + 1) % SIZE;
     }
     printf("\n");
 }
 
-/* ---------------- MAIN FUNCTION ---------------- */
+//  ---------------- MAIN FUNCTION ---------------- 
 
-int main() {
+int main()
+{
     int choice, value;
 
-    while (1) {
+    while (1)
+    {
         printf("\n===== AIRPORT AUTOMATION MENU =====\n");
         printf("\nFor Luggage Operations ->\n");
         printf("1. Push Luggage\n");
@@ -124,49 +156,51 @@ int main() {
         printf("\nEnter choice: ");
         scanf("%d", &choice);
 
-        switch (choice) {
-            case 1:
-                printf("Enter luggage tag: ");
-                scanf("%d", &value);
-                push(value);
-                break;
+        switch (choice)
+        {
+        case 1:
+            printf("Enter luggage tag: ");
+            scanf("%d", &value);
+            push(value);
+            break;
 
-            case 2:
-                pop();
-                break;
+        case 2:
+            pop();
+            break;
 
-            case 3:
-                peekStack();
-                break;
+        case 3:
+            peekStack();
+            break;
 
-            case 4:
-                displayStack();
-                break;
+        case 4:
+            displayStack();
+            break;
 
-            case 5:
-                printf("Enter passenger ID: ");
-                scanf("%d", &value);
-                enqueue(value);
-                break;
+        case 5:
+            printf("Enter passenger ID: ");
+            scanf("%d", &value);
+            enqueue(value);
+            break;
 
-            case 6:
-                dequeue();
-                break;
+        case 6:
+            dequeue();
+            break;
 
-            case 7:
-                peekQueue();
-                break;
+        case 7:
+            peekQueue();
+            break;
 
-            case 8:
-                displayQueue();
-                break;
+        case 8:
+            displayQueue();
+            break;
 
-            case 9:
-                printf("Program Ended.\n");
-                return 0;
+        case 9:
+            printf("Program Ended.\n");
+            exit(0);
 
-            default:
-                printf("Invalid choice! Try again.\n");
+        default:
+            printf("Invalid choice! Try again.\n");
         }
     }
+    return 0;
 }
